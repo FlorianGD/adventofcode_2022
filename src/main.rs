c4c26d601eb62c7da814313a51167aed6478f522
Florian Gaudin-Delrieu <9217921+FlorianGD@users.noreply.github.com>
2022-12-03 12:04:52 +0000

    feat(day03) : both parts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use adventofcode_2022::{day01, day02};
+use adventofcode_2022::{day01, day02, day03};
 use anyhow::Result;
-use aoc_next::{aoc_main, failable_parser, solution, solver, Aoc};
+use aoc_next::{aoc_main, failable_parser, parser, solution, solver, Aoc};
 
 const AOC: Aoc = Aoc {
     allow_download: true,
@@ -10,6 +10,8 @@ const AOC: Aoc = Aoc {
         solution! {1, failable_parser!{ day01::parse_input }, solver!{ day01::part2 }},
         solution! {2, failable_parser!{ day02::parse_input }, solver!{ day02::part1 }},
         solution! {2, failable_parser!{ day02::parse_input }, solver!{ day02::part2 }},
+      solution! {3, parser!{ day03::parse_input }, solver!{ day03::part1 }},
+      solution! {3, parser!{ day03::parse_input_p2 }, solver!{ day03::part2 }},
     ],
 };
 
