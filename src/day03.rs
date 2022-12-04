9f14bfec59cdcb2d8bbf4b9148a933509bf2334d
Florian Gaudin-Delrieu <9217921+FlorianGD@users.noreply.github.com>
2022-12-04 11:31:35 +0000

    feat(day03) : both parts

--- src/day03.rs
+++ src/day03.rs
@@ -1,54 +1,67 @@
-use std::collections::HashSet;
 use itertools::Itertools;
+use std::collections::HashSet;
 
-pub fn parse_input(input : & str) -> Vec<(String, String)> {
-//  let input = "vJrwpWtwJgWrhcsFMMfFFhFp
-//jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-//PmmdzqPrVvPwwTWBwg
-//wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-//ttgJtRGJQctTZtZT
-//CrZsJsPPZsGzwwsLwLmpwMDw";
-  input.lines().map(|line| {
-    let half = line.len() / 2; // OK because ASCII
-    (line[..half].to_string(), line[half..].to_string())
-  }).collect() 
+pub fn parse_input(input: &str) -> Vec<(String, String)> {
+    //  let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+    //jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    //PmmdzqPrVvPwwTWBwg
+    //wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    //ttgJtRGJQctTZtZT
+    //CrZsJsPPZsGzwwsLwLmpwMDw";
+    input
+        .lines()
+        .map(|line| {
+            let half = line.len() / 2; // OK because ASCII
+            (line[..half].to_string(), line[half..].to_string())
+        })
+        .collect()
 }
 
-pub fn part1(input: Vec<(String, String)>)-> u32 {
-  input.into_iter().map(|(l,r)| {
-    let set : HashSet<char> =l.chars().collect();
-    for c in r.chars() {
-      if set.contains(&c) { return c}
-    };
-    unreachable! ("no common char");
-  }).map(|c| {
-    match c {
-      'a'..='z' => (c as u32 - 'a' as u32) +1, 
-      'A'..='Z' => (c as u32 - 'A' as u32) +26+1, 
-      _ => unreachable!("wrong input")
-    }
-  }).sum()
+pub fn part1(input: Vec<(String, String)>) -> u32 {
+    input
+        .into_iter()
+        .map(|(l, r)| {
+            let set: HashSet<char> = l.chars().collect();
+            for c in r.chars() {
+                if set.contains(&c) {
+                    return c;
+                }
+            }
+            unreachable!("no common char");
+        })
+        .map(|c| match c {
+            'a'..='z' => (c as u32 - 'a' as u32) + 1,
+            'A'..='Z' => (c as u32 - 'A' as u32) + 26 + 1,
+            _ => unreachable!("wrong input"),
+        })
+        .sum()
 }
 
-pub fn parse_input_p2(input: &str) -> Vec<char>{
-  input.lines().chunks(3).into_iter().map(|mut chunk|{
-    let s: HashSet<char> = chunk.next().unwrap().  chars().collect();
-    let s2 : HashSet<char> = chunk.next().unwrap().chars().collect();
-    for c in chunk.next().unwrap().chars() {
-      if (s.intersection(&s2)).contains(&c) {
-        return c
-      }
-    }
-    unreachable! ("no common char");
-  }).collect()
+pub fn parse_input_p2(input: &str) -> Vec<char> {
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|mut chunk| {
+            let s: HashSet<char> = chunk.next().unwrap().chars().collect();
+            let s2: HashSet<char> = chunk.next().unwrap().chars().collect();
+            for c in chunk.next().unwrap().chars() {
+                if (s.intersection(&s2)).contains(&c) {
+                    return c;
+                }
+            }
+            unreachable!("no common char");
+        })
+        .collect()
 }
 
-pub fn part2(input: Vec<char>)-> u32 {
-  input.into_iter().map(|c| {
-    match c {
-      'a'..='z' => (c as u32 - 'a' as u32) +1, 
-      'A'..='Z' => (c as u32 - 'A' as u32) +26+1, 
-      _ => unreachable!("wrong input")
-    }
-  }).sum()
-} 
+pub fn part2(input: Vec<char>) -> u32 {
+    input
+        .into_iter()
+        .map(|c| match c {
+            'a'..='z' => (c as u32 - 'a' as u32) + 1,
+            'A'..='Z' => (c as u32 - 'A' as u32) + 26 + 1,
+            _ => unreachable!("wrong input"),
+        })
+        .sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use adventofcode_2022::{day01, day02, day03};
+use adventofcode_2022::{day01, day02, day03, day04};
 use anyhow::Result;
 use aoc_next::{aoc_main, failable_parser, parser, solution, solver, Aoc};
 
 const AOC: Aoc = Aoc {
-    allow_download: true,
+    allow_download: false,
     year: 2022,
     solutions: &[
         solution! {1, failable_parser!{ day01::parse_input }, solver!{ day01::part1 }},
         solution! {1, failable_parser!{ day01::parse_input }, solver!{ day01::part2 }},
         solution! {2, failable_parser!{ day02::parse_input }, solver!{ day02::part1 }},
         solution! {2, failable_parser!{ day02::parse_input }, solver!{ day02::part2 }},
-      solution! {3, parser!{ day03::parse_input }, solver!{ day03::part1 }},
-      solution! {3, parser!{ day03::parse_input_p2 }, solver!{ day03::part2 }},
+        solution! {3, parser!{ day03::parse_input }, solver!{ day03::part1 }},
+        solution! {3, parser!{ day03::parse_input_p2 }, solver!{ day03::part2 }},
+        solution! {4, failable_parser!{ day04::parse_input }, solver!{ day04::part1 }},
+        solution! {4, failable_parser!{ day04::parse_input }, solver!{ day04::part2 }},
     ],
 };
 
