fa2e69ea2effc83d5bdbb4cb3d0aab59571d1d92
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2022-12-22 19:02:21 +0000

    wip(day17 part 2)

--- src/day17.rs
+++ src/day17.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use anyhow::{anyhow, Result};
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
 pub enum Direction {
     Left,
     Right,
@@ -19,10 +19,11 @@ impl Direction {
 }
 
 pub fn parse_input(input: &str) -> Result<Vec<Direction>> {
+    // let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
     input.trim().chars().map(Direction::from_char).collect()
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Board {
     lines: Vec<u8>,
 }
@@ -98,9 +99,23 @@ impl Board {
         }
         self.settle_block_at(&mut block, offset);
     }
+
+    /// Return the floor for each index, i.e. the first where it is 1.
+    /// Safety: the first is 127, so we can unwrap safely
+    fn floor(&self) -> Vec<usize> {
+        (0..7)
+            .map(|i| {
+                self.lines
+                    .iter()
+                    .rev()
+                    .position(|&x| x & (1 << i) != 0)
+                    .unwrap()
+            })
+            .collect()
+    }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Block {
     shape: Vec<u8>,
 }
@@ -199,9 +214,52 @@ pub fn part1(directions: Vec<Direction>) -> usize {
     for block in blocks {
         board.fall_block(block, &mut dir);
     }
-    println!("{:?}", board);
     board.lines.len() - 1
 }
+
+pub fn part2(directions: Vec<Direction>) -> usize {
+    let b1 = Block { shape: vec![30] };
+    let b2 = Block {
+        shape: vec![8, 28, 8],
+    };
+    let b3 = Block {
+        shape: vec![28, 4, 4],
+    };
+    let b4 = Block {
+        shape: vec![16, 16, 16, 16],
+    };
+    let b5 = Block {
+        shape: vec![24, 24],
+    };
+    let dir_len = &directions.len() * 5; // LCM #directions #shapes
+    const ITERATIONS: usize = 1000000000000;
+    // const ITERATIONS: usize = 10000;
+    let mut dir = directions.into_iter().cycle();
+    let blocks = vec![b1, b2, b3, b4, b5].into_iter().cycle();
+    // cache is index modulo dir_len, floor, total blocks
+    let mut cache: Vec<(usize, Vec<usize>, usize)> = Vec::new();
+    let mut board = Board::new();
+    for (i, block) in blocks.enumerate() {
+        board.fall_block(block, &mut dir);
+        let floor = board.floor();
+        match cache
+            .iter()
+            .position(|(j, x, _)| *j == (i % dir_len) && *x == floor)
+        {
+            None => cache.push((i % dir_len, floor.clone(), board.lines.len() - 1)),
+            Some(p) => {
+                let block_added_in_cycle: usize = cache[i - 1].2 - cache[p - 1].2;
+                let (div, modulo) = ((ITERATIONS - p) / (i - p), (ITERATIONS - p) % (i - p));
+                // Add the modulo - the beginning steps
+                let remaining_blocks: usize = cache[p + modulo].2 - cache[p].2;
+                let beginning_blocks: usize = cache[p - 1].2 + 1;
+                return beginning_blocks + block_added_in_cycle * div + remaining_blocks;
+            }
+        }
+    }
+    unreachable!()
+}
+//1524110593571 too low for part 2
 
 #[cfg(test)]
 mod test {
@@ -362,6 +420,14 @@ mod test {
     fn test_part1() {
         if let Ok(directions) = parse_input(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>") {
             assert_eq!(part1(directions), 3068)
+        } else {
+            panic!()
+        }
+    }
+    #[test]
+    fn test_part2() {
+        if let Ok(directions) = parse_input(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>") {
+            assert_eq!(part2(directions), 1514285714288)
         } else {
             panic!()
         }
