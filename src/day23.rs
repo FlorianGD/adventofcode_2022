31b438c8b3a377d8dd8445003fcbcf4e9ff02f6d
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2023-01-13 15:16:58 +0000

    enh(clippy)

--- src/day23.rs
+++ src/day23.rs
@@ -49,7 +49,7 @@ fn has_neighbor(e: &Pos, elves: &Elves) -> bool {
             return true;
         }
     }
-    return false;
+    false
 }
 
 fn propose_moves(vec_elves: &VecElves, elves: &Elves, neighbors: &VecDeque<Neighbors>) -> VecElves {
@@ -80,7 +80,7 @@ fn round(elves: &Elves, neighbors: &mut VecDeque<Neighbors>) -> Elves {
     let proposed = propose_moves(&vec_elves, elves, neighbors);
     neighbors.rotate_left(1);
     elves
-        .into_iter()
+        .iter()
         .zip(proposed.iter())
         .map(|(initial, prop)| {
             if proposed.iter().filter(|&x| x == prop).count() > 1 {
@@ -90,44 +90,6 @@ fn round(elves: &Elves, neighbors: &mut VecDeque<Neighbors>) -> Elves {
             }
         })
         .collect()
-}
-
-fn _draw_elves(elves: &Elves) {
-    let x_min = elves
-        .iter()
-        .min_by(|x, y| x.re.cmp(&y.re))
-        .unwrap()
-        .re
-        .min(0);
-    let x_max = elves
-        .iter()
-        .max_by(|x, y| x.re.cmp(&y.re))
-        .unwrap()
-        .re
-        .max(10);
-    let y_min = elves
-        .iter()
-        .min_by(|x, y| x.im.cmp(&y.im))
-        .unwrap()
-        .im
-        .min(0);
-    let y_max = elves
-        .iter()
-        .max_by(|x, y| x.im.cmp(&y.im))
-        .unwrap()
-        .im
-        .max(10);
-    for j in y_min..=y_max {
-        for i in x_min..=x_max {
-            if elves.contains(&Complex::new(i, j)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-    println!();
 }
 
 fn score(elves: &Elves) -> isize {
@@ -153,11 +115,9 @@ pub fn part1(elves: Elves) -> isize {
         ),
         (Complex::new(1, -1), Complex::new(1, 0), Complex::new(1, 1)),
     ]);
-    // _draw_elves(&elves);
     let mut elves = elves;
     for _ in 0..10 {
         let new_elves = round(&elves, &mut neighbors);
-        // _draw_elves(&new_elves);
         if elves == new_elves {
             // none moved, the process is over
             break;
@@ -182,13 +142,11 @@ pub fn part2(elves: Elves) -> isize {
         ),
         (Complex::new(1, -1), Complex::new(1, 0), Complex::new(1, 1)),
     ]);
-    // _draw_elves(&elves);
     let mut elves = elves;
     let mut i = 1;
 
     loop {
         let new_elves = round(&elves, &mut neighbors);
-        // _draw_elves(&new_elves);
         if elves == new_elves {
             // none moved, the process is over
             return i;
